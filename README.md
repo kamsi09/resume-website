71c3a97bf487ae00359c0e9b4925f2709bd76410
kamsi09 <kamsioduche85@gmail.com>
2019-07-12 23:46:13 -0500

    redo  initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+My Portfolio website using the ReactJS framework
+
+## How to use
+1. Clone/Download the repo.
+2. Run  ``` npm install ```.
+3. Run ```npm start``` to spin the up the local dev server port 3000.(http://localhost:3000).
+
---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://sam.oduche.com",
+    "name": "Sam Oduche",
+    "role": "Software Engineer",
+    "linkedinId":"in/sam-oduche-279610153/",
+    "roleDescription": "I aspire to constantly learn, innovate and grow so I use software development as a way to always be a part of the chase. In my free time I like to stay active so I play a lot of basketball and compete in pool competitions",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sam-oduche-279610153/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/kamsi09",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a 4th year CS student at Texas Tech university pursuing my BS. I am starting my journey to becoming a Full Stack Software Engineer so I am currently diving into programming and scripting languages such as PHP, Python and C#.",
+    "email":"sam.oduche@ttu.edu",
+    "address":"Lubbock, TX",
+    "website":"https://github.com/kamsi09",
+    "education":[
+      {
+        "UniversityName":"Texas Tech University",
+        "specialization":"BS Computer Science - Math Minor",
+        "MonthOfPassing":"December",
+        "YearOfPassing":"2020",
+        "Achievements": [{
+          "firstAchievement":"Deans List",
+          "secondAchievement":"Complex Concil - Vice President of Executive Development",
+          "thirdAchievement":"National Society of Black Engineers (NSBE)",
+        }],
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"USAA",
+        "specialization":"Software Development and Integration Intern",
+        "MonthOfLeaving":"May - August",
+        "YearOfLeaving":"2019",
+        "Achievements": [{
+          "firstAchievement":"Worked in a full-stack environment to develop a web application using a relational database with API integration alongside a UI developed in ReactJS and Redux",
+          "secondAchievement":"Participated in innovation challenges and hackathons to modernize internal and external facing systems",
+          "thirdAchievement":"",
+        }]
+      },
+      {
+        "CompanyName":"USAA",
+        "specialization":"Software Development and Integration Intern",
+        "MonthOfLeaving":"May - August",
+        "YearOfLeaving":"2018",
+        "Achievements": [{
+          "firstAchievement":"Worked in a back-end environment to develop an API and implement new REST services.",
+          "secondAchievement":"Worked in a back-end environment to develop an API and implement new REST services.",
+          "thirdAchievement":"",
+        }]
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
