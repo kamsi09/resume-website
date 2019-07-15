edd766d2a2231010ce63b4e82a907dbf73d02608
kamsi09 <kamsioduche85@gmail.com>
2019-07-14 20:31:18 -0500

    save

--- src/resumeData.js
+++ src/resumeData.js
@@ -97,7 +97,7 @@ let resumeData = {
   "portfolio": [
     {
       "name": "Resume",
-      "description": "A simple webpage explicitly built using Reactjs and the FontAwesome library",
+      "description": "A simple webpage explicitly built using only Reactjs and CSS",
       "imgurl": "images/portfolio/website.png",
       "tools": "Website Portfolio",
       "gitHubLink": "https://github.com/kamsi09/resume-website"
