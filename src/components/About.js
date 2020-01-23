4a94e08d2f5bede7d2949c14b065b869f2d3ffeb
kamsi09 <kamsioduche85@gmail.com>
2020-01-23 13:31:34 -0600

    Update format

--- src/components/About.js
+++ src/components/About.js
@@ -1,45 +1,37 @@
 import React, { Component } from 'react';
 export default class About extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
-    return (
-      <section id="about">
-         <div className="row">
-
-            <div className="three columns">
-
-               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
-
-            </div>
-
-            <div className="nine columns main-col">
-
-               <h2>About Me</h2>
-               <p>
-               {
-                 resumeData.aboutme
-               }
-               </p>
-
-               <div className="row">
-
-                  <div className="columns contact-details">
-
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                     <span>Email : <a href="mailto:sam.oduche@ttu.edu">{resumeData.email}</a></span>
-                     <br></br>
-       						   <span>
-                     Location : {resumeData.address}
-                    </span>
-                    <br></br>
-                    <span><u><a href="https://github.com/kamsi09">{resumeData.website}</a></u></span>
-       					   </p>
+   render() {
+      let resumeData = this.props.resumeData;
+      return (
+         <section id="about">
+            <div className="row">
+               <div className="three columns">
+                  <img className="profile-pic" src="images/profilepic.jpg" alt="" />
+               </div>
+               <div className="nine columns main-col">
+                  <h2>About Me</h2>
+                  <p>
+                     {
+                        resumeData.aboutme
+                     }
+                  </p>
+                  <div className="row">
+                     <div className="columns contact-details">
+                        <h2>Contact Details</h2>
+                        <p className="address">
+                           <span>Email : <a href="mailto:sam.oduche@ttu.edu">{resumeData.email}</a></span>
+                           <br></br>
+                           <span>
+                              Location : {resumeData.address}
+                           </span>
+                           <br></br>
+                           <span><u><a href="https://github.com/kamsi09">{resumeData.website}</a></u></span>
+                        </p>
+                     </div>
                   </div>
                </div>
             </div>
-         </div>
-      </section>
-    );
-  }
+         </section>
+      );
+   }
 }
